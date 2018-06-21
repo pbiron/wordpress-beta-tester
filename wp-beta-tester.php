8e9dccd1cbcc59e67537037d0ca135de90f2faea
Andy Fragen <andy@thefragens.com>
2018-06-21 09:37:18 -0700

    tabs not spaces

--- wp-beta-tester.php
+++ wp-beta-tester.php
@@ -12,7 +12,7 @@
 */
 
 /*
-  Copyright 2009-2016 Peter Westwood (email : peter.westwood@ftwr.co.uk)
+	Copyright 2009-2016 Peter Westwood (email : peter.westwood@ftwr.co.uk)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as

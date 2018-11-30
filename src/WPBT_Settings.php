de9ffd4e056b896e894c2961d43d7d0c1bd85919
Andy Fragen <andy@thefragens.com>
2018-11-30 11:44:25 -0800

    fixed to automatically load correct tab

--- src/WPBT_Settings.php
+++ src/WPBT_Settings.php
@@ -102,7 +102,7 @@ class WPBT_Settings {
 			echo '<div id="message" class="error"><p>';
 			$admin_page = is_multisite() ? 'settings.php' : 'tools.php';
 			/* translators: %s: link to setting page */
-			printf( wp_kses_post( __( '<strong>Error:</strong> Your current <a href="%s">WordPress Beta Tester plugin configuration</a> will downgrade your install to a previous version - please reconfigure it.', 'wordpress-beta-tester' ) ), admin_url( $admin_page . '?page=wp_beta_tester' ) );
+			printf( wp_kses_post( __( '<strong>Error:</strong> Your current <a href="%s">WordPress Beta Tester plugin configuration</a> will downgrade your install to a previous version - please reconfigure it.', 'wordpress-beta-tester' ) ), admin_url( $admin_page . '?page=wp_beta_tester&tab=wp_beta_tester_core' ) );
 			echo '</p></div>';
 		}
 	}

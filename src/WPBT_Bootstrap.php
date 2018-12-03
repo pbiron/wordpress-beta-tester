8cc060fd9547d702eaaf494e9b376cce6a93846c
Andy Fragen <andy@thefragens.com>
2018-12-03 09:29:14 -0800

    use composer autoloader

--- src/WPBT_Bootstrap.php
+++ src/WPBT_Bootstrap.php
@@ -17,7 +17,6 @@ class WPBT_Bootstrap {
 	 */
 	public function run() {
 		add_action( 'init', array( $this, 'load_textdomain' ) );
-		$this->load_requires();
 		$wpbt = new WP_Beta_Tester();
 		$wpbt->run();
 		// TODO: I really want to do this, but have to wait for PHP 5.4
@@ -31,17 +30,5 @@ class WPBT_Bootstrap {
 	 */
 	public function load_textdomain() {
 		load_plugin_textdomain( 'wordpress-beta-tester' );
-	}
-
-	/**
-	 * <sarcasm>Poor man's autoloader.</sarcasm>
-	 *
-	 * @return void
-	 */
-	protected function load_requires() {
-		require_once WP_BETA_TESTER_DIR . '/src/WP_Beta_Tester.php';
-		require_once WP_BETA_TESTER_DIR . '/src/WPBT_Settings.php';
-		require_once WP_BETA_TESTER_DIR . '/src/WPBT_Core.php';
-		require_once WP_BETA_TESTER_DIR . '/src/WPBT_Extras.php';
 	}
 }
--- wp-beta-tester.php
+++ wp-beta-tester.php
@@ -27,7 +27,8 @@ define( 'WP_BETA_TESTER_DIR', dirname( __FILE__ ) );
 add_action( 'plugins_loaded', 'load_beta_tester_plugin' );
 
 function load_beta_tester_plugin() {
-	require_once WP_BETA_TESTER_DIR . '/src/WPBT_Bootstrap.php';
+	require_once WP_BETA_TESTER_DIR . '/vendor/autoload.php';
+	//require_once WP_BETA_TESTER_DIR . '/src/WPBT_Bootstrap.php';
 	$wp_beta_tester_bootstrap = new WPBT_Bootstrap();
 	$wp_beta_tester_bootstrap->run();
 	// TODO: I really want to do this, but have to wait for PHP 5.4

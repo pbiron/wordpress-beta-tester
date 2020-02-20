70517b4feeddadd6a7c9f769dd5f6d6bf8aa2057
pedro-mendonca <ped.gaspar@gmail.com>
2020-02-20 12:03:08 +0000

    Fix strings hardcoded concatenation issues

--- src/WPBT/WPBT_Core.php
+++ src/WPBT/WPBT_Core.php
@@ -158,7 +158,7 @@ class WPBT_Core {
 		echo '</p><p>';
 		printf(
 			/* translators: %s: update version */
-			wp_kses_post( __( 'Currently your site is set to update to version %s.', 'wordpress-beta-tester' ) ),
+			wp_kses_post( __( 'Currently your site is set to update to %s.', 'wordpress-beta-tester' ) ),
 			'<strong>' . esc_attr( $preferred->version ) . '</strong>'
 		);
 		echo '</p><p>';
@@ -207,7 +207,7 @@ class WPBT_Core {
 		<?php if ( $unstable && $beta_rc && $show_beta_rc ) : ?>
 		<tr>
 			<th><label><input name="wp-beta-tester" id="update-stream-beta-rc-unstable"    type="radio" value="beta-rc-unstable" class="tog" <?php checked( 'beta-rc-unstable', self::$options['stream'] ); ?> />
-			<?php esc_html_e( 'Beta/RC - Bleeding edge ', 'wordpress-beta-tester' ); ?>
+			<?php esc_html_e( 'Beta/RC - Bleeding edge', 'wordpress-beta-tester' ); ?>
 			</label></th>
 			<td><?php echo( wp_kses_post( __( 'This is for the Beta/RC releases only of development code from `trunk`. It will only update to beta/RC releases of `trunk`.', 'wordpress-beta-tester' ) ) ); ?></td>
 		</tr>
@@ -250,13 +250,13 @@ class WPBT_Core {
 				! preg_match( '/alpha|beta|RC/', get_bloginfo( 'version' ) ) ) ) {
 			// site is not running a development version or not on a beta/RC stream.
 			// So use the preferred version.
-			return $preferred_version;
+			return sprintf( __( 'version %s', 'wordpress-beta-tester' ), $preferred_version );
 		}
 
 		$next_version = $this->wp_beta_tester->beta_rc->get_found_version();
 		if ( $next_version ) {
 			// the next beta/RC package was found, return that version.
-			return $next_version;
+			return sprintf( __( 'version %s', 'wordpress-beta-tester' ), $next_version ); ;
 		}
 
 		// the next beta/RC package was not found.
@@ -267,7 +267,7 @@ class WPBT_Core {
 			$next_version = __( 'next development version', 'wordpress-beta-tester' );
 		} else {
 			// show all versions that may come next.
-			$next_version = implode( ' or ', $next_version ) . ', ' . __( 'whichever is released first', 'wordpress-beta-tester' );
+			$next_version = wp_sprintf( __( 'version %l', 'wordpress-beta-tester' ), $next_version ) . ', ' . __( 'whichever is released first', 'wordpress-beta-tester' );
 		}
 
 		return $next_version;
